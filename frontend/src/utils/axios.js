666778fcbfc754faa79121bac1af6f20952cb974
Gagan3287 <tejchandra100@gmail.com>
2026-09-24 17:11:43 +0530

    add deployed backend url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import qs from 'qs';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://homelyhub-internship-batch8.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://homelyhubinternshipbatch8-ycne.onrender.com/api',
     withCredentials: true,
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 })
